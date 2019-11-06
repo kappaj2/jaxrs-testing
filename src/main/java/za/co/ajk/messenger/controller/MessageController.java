8b05fe13483fdfa2128eb382ab17bbbd3658edbc
Andre <kappaj@gmail.com>
2019-11-06 15:12:06 +0200

    Adding POST method

--- src/main/java/za/co/ajk/messenger/controller/MessageController.java
+++ src/main/java/za/co/ajk/messenger/controller/MessageController.java
@@ -4,7 +4,10 @@ import jdk.nashorn.internal.objects.annotations.Getter;
 import za.co.ajk.messenger.model.Message;
 import za.co.ajk.messenger.service.MessageService;
 
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -29,6 +32,12 @@ public class MessageController {
         return messageService.getMessage(messageId);
     }
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Message createMessage(Message message){
+        return messageService.addMessage(message);
+    }
 
 //    @GET
 //    @Path("/getJSON")
